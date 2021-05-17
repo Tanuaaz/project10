c1735e54c035b69c3858c6e61093643172cc62b2
Tanuaaz <t.zubareva@g.nsu.ru>
2021-05-17 08:43:59 +0700

    menu

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+'''This program helps to make shopping list'''
 import ru_local
 
+#making the shopping list dictionary
 shopping_basket = {}
 print(ru_local.OPTION)
 
 option = int(input(ru_local.MENU))
 
+
 while option != 0:
-    if option == 1:
+    if option == 1: #checking menu
         item = input(ru_local.ITEM)
 
-        if item in shopping_basket:
+        if item in shopping_basket: #checking if item already in bakset
             print(ru_local.ALREADY)
             count = int(input(ru_local.COUNT))
             shopping_basket[item] = shopping_basket[item] + count
